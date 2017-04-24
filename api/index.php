e2786132f0d8178339c7cb01bb38fdbf23fb8852
ioCron <info@iocron.com>
2017-04-24 22:59:13 +0200

    Moved api from resources to root

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../resources/lib/Helper.php");
+require_once("../resources/lib/Deployer.php");
+require_once("../resources/lib/Api.php");
+
+$api = new Api($_POST);
--- resources/api/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../lib/Helper.php");
-require_once("../lib/Deployer.php");
-require_once("../lib/Api.php");
-
-$api = new Api($_POST);
