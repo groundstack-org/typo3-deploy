e8539d23f417a1c6bea4d1e7907572ed12496a11
Christian Hackl <hackl.chris@googlemail.com>
2017-11-02 11:48:13 +0100

    bugfix comment addconfig.php

--- resources/lib/Deployer.php
+++ resources/lib/Deployer.php
@@ -283,10 +283,10 @@ if (!defined('TYPO3_MODE')) {
 }
 /*
   !IMPORTANT!
-  The file "\$databaseCredentialsFile" is not versioned and must be created and changed!
+  The file '\$databaseCredentialsFile' is not versioned and must be created and changed!
   For security reasons please create the database accesses outside the document roots!
 
-  Die Datei "\$databaseCredentialsFile" wird nicht mit Versioniert und muss extra angelegt und geändert werden!
+  Die Datei '\$databaseCredentialsFile' wird nicht mit Versioniert und muss extra angelegt und geändert werden!
   aus Sicherheitsgründen bitte die Datenbank zugänge außerhalb des document roots anlegen!
 */
 
